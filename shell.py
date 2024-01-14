510f23b3268101efc1e83de5b94507a3481188ee
Seamus Mullan <43112447+SeamusMullan@users.noreply.github.com>
2024-01-14 21:21:16 +0000

    just followed python compiler tutorial. No custom edits made to language

--- shell.py
+++ shell.py
@@ -1,10 +1,14 @@
 import carrot
 
 while True:
-    text = input('carrot> ')
-    result, error = carrot.run('fileName', text)
+	text = input('CarrotLang > ')
+	if text.strip() == "": continue
+	result, error = carrot.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
-
-    
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
